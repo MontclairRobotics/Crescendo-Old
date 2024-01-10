9d1de70c0cefe7fd39c885178928e70a51c47c6a
Leo Fitzpatrick <leocfitz@gmail.com>
2024-01-10 15:38:20 -0500

    added template code for the flippiest fliptop

---
+++ src/main/java/org/team555/components/subsystems/Fliptop.java
@@ -0,0 +1,24 @@
+package org.team555.components.subsystems;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import org.team555.constants.*;
+
+public class Fliptop {
+    //TODO: Actual motor ID
+    public CANSparkMax motor = new CANSparkMax(Constants.FLIPTOP_MOTOR_ID, MotorType.kBrushless);
+    
+    public Fliptop() {
+        motor.setInverted(false);
+    }
+    
+    public void forward() {
+        motor.set(Constants.FLIPTOP_SPEED);
+    }
+    public void backward() {
+        motor.set(-1*Constants.FLIPTOP_SPEED);
+    }
+    public void off() {
+        motor.set(0);
+    }
+    
+}
--- src/main/java/org/team555/constants/Constants.java
+++ src/main/java/org/team555/constants/Constants.java
@@ -1,5 +1,8 @@
 package org.team555.constants;
 
 public class Constants {
+    //TODO: Actual MOTOR PORT
+    public static int FLIPTOP_MOTOR_ID = -1; 
+    public static double FLIPTOP_SPEED = 0.5;
     
 }
