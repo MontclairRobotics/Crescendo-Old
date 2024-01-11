5348dc6779ecfaca5a5cf7d4c3c7d0f16c0d6809
Voxel <jamesgormleyrechs@gmail.com>
2024-01-11 14:48:01 -0500

    Merged branches (#2)
    * Merged branches
    
    Resolved all merge conflicts between the different branches, fixed bugs, formatted, renamed ports/constants
    
    * Set Anglemove motor to final
    
    Set anglemove motor to final.
    
    * Update Shooter.java
    
    * Updated AngleMove
    
    Updated Anglemove to Sprocket to match a change in robot design
    
    * Described gradle bug
    
    added comment regarding gradle bug "unresolved dependency: org.junit.platform"
    
    Possibly a vscode error, check pull request #2 for details.
    
    * Updated Vendordep
    
    Updated SdsSwerveLib vendordep from 2024.0.1.0 -> 2024.0.2.0
    
    refer to:
    
    https://github.com/democat3457/swerve-lib/releases/tag/2024.0.2.0

--- src/main/java/org/team555/Commands555.java
+++ src/main/java/org/team555/Commands555.java
@@ -1,7 +1,54 @@
 package org.team555;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 
 public class Commands555 
 {   
+    // INTAKE COMMANDS
+
+    public static Command eat() {
+        return Commands.runOnce(Crescendo.mouth::in, Crescendo.mouth).withName("intake in");
+    }
+
+    public static Command barf() {
+       return Commands.runOnce(Crescendo.mouth::out, Crescendo.mouth).withName("intake out");
+    }
+
+    public static Command stopIntake() {
+        return Commands.runOnce(Crescendo.mouth::stop, Crescendo.mouth).withName("intake stop");
+    }
+
+    // FLIPTOP COMMANDS
+
+    public Command foward() {
+        return Commands.runOnce(Crescendo.fliptop::forward, Crescendo.fliptop).withName("fliptop forward");
+    }
+
+    public Command backward() {
+        return Commands.runOnce(Crescendo.fliptop::backward, Crescendo.fliptop).withName("fliptop backward");
+    }
+
+    // SPROCKET COMMANDS
+
+    public static Command goUp() {
+        return Commands.runOnce(Crescendo.sprocket::goUp).withName("sprocket up");
+    }
+    public static Command goDown() {
+        return Commands.runOnce(Crescendo.sprocket::goDown).withName("sprocket down");
+    }
+    public static Command stopAngleMove() {
+        return Commands.runOnce(Crescendo.sprocket::stop).withName("sprocket stop");
+    }
     
+
+    // SHOOTER COMMANDS
+
+    public Command shoot() {
+        return Commands.runOnce(Crescendo.shooter::shoot, Crescendo.shooter).withName("shoot");
+    }
+    
+    public Command stopShooter() {
+        return Commands.runOnce(Crescendo.shooter::stop, Crescendo.shooter).withName("stop");
+    }
 }
