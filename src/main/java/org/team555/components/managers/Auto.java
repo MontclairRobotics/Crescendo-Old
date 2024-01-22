1613776c131dcbf2579fdb00a97e855376e73d13
Danica Stout <danicastout11@gmail.com>
2024-01-22 15:44:42 -0500

    Started writing auto code!

--- src/main/java/org/team555/components/managers/Auto.java
+++ src/main/java/org/team555/components/managers/Auto.java
@@ -5,13 +5,21 @@ import org.team555.components.subsystems.Drivetrain;
 import org.team555.constants.DriveConstants;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
 import com.pathplanner.lib.util.ReplanningConfig;
 
+import edu.wpi.first.wbilibj.shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
+
 public class Auto { 
 
-    public Auto() {
+    private ShuffleboardTab autoTab = Shuffleboard.getTab("auto");
+    private SendableChooser<String> chooseStart;
 
+    public Auto() {
         AutoBuilder.configureHolonomic(
             Crescendo.drivetrain::getRobotPose, 
             Crescendo.drivetrain::setRobotPose, 
@@ -25,7 +33,23 @@ public class Auto {
                 new ReplanningConfig()
             ), 
             Crescendo.drivetrain::shouldFlipSide, 
-            Crescendo.drivetrain)
-        ;
+            Crescendo.drivetrain
+        );
+
+        autoTab.add("Starting Position", chooseStart)
+            .withPosition(0,0)
+            .withSize(2, 1);
+
+    }
+
+    /**
+     * Gets the path from path planner 
+     * @param filePath (String)
+     * @return the command to follow the path it just got.
+     */
+    public Command getPath(String filePath) {
+        PathPlannerPath path = new PathPlannerPath.fromPathFile(filePath);
+
+        return AutoBuilder.followPath(path);
     }
 }
